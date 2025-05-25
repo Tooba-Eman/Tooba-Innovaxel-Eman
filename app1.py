f81ddacbcdff46d4089803088ca77b3dace3f6a9
Tooba-Eman <emantooba554@gmail.com>
2025-05-25 10:10:24 +0500

    Add HTML template for visualization

--- app1.py
+++ app1.py
@@ -1,3 +1,5 @@
+
+from flask import Flask, request, jsonify, redirect, render_template
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,6 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "Welcome to Innovaxel Project!"
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+app.run(debug=True, port=5000)
