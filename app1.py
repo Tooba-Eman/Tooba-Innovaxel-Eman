02105391a68a9663184fa2b29f6d607e1277d270
Tooba-Eman <emantooba554@gmail.com>
2025-05-25 11:31:00 +0500

    Added delete URL logic

--- app1.py
+++ app1.py
@@ -1,27 +1,14 @@
-url['id'] = str(url['_id'])
-return jsonify(url)
+updated["id"] = str(updated["_id"])
+return jsonify(updated)
 
 
-# Update Long URL based on short url
-@app.route('/shorten/<short_code>', methods=['PUT'])
-def update_url(short_code):
-    data = request.json
-    new_url = data.get("url")
-    if not new_url:
-        return jsonify({"error": "URL is required"}), 400
-
-    url = db.urls.find_one({"shortCode": short_code})
-    if not url:
+@app.route('/shorten/<short_code>', methods=['DELETE'])
+def delete_url(short_code):
+    result = db.urls.delete_one({"shortCode": short_code})
+    if result.deleted_count == 0:
         return jsonify({"error": "Short URL not found"}), 404
+    return '', 204
 
-    db.urls.update_one(
-        {"shortCode": short_code},
-        {"$set": {"url": new_url, "updatedAt": datetime.utcnow().isoformat()}}
-    )
-
-    updated = db.urls.find_one({"shortCode": short_code})
-    updated["id"] = str(updated["_id"])
-    return jsonify(updated)
 
 if __name__ == '__main__':
 app.run(debug=True, port=5000)
