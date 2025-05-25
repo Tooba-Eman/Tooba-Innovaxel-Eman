d8db97c9ea3c5155bba797f6bd8ca2e0b648087a
Tooba-Eman <emantooba554@gmail.com>
2025-05-25 13:35:26 +0500

    Implement redirect logic for short URLs

--- app1.py
+++ app1.py
@@ -1,49 +1,55 @@
-from datetime import datetime
-import random
-import string
-from urllib.parse import urlparse
-
-
-app = Flask(__name__)
-CORS(app)
-@@ -19,15 +21,32 @@ def generate_short_code(length=6):
-def home():
-return render_template('index.html')
-
-
-def is_valid_url(url):
-    try:
-        if ' ' in url:
-            return False
-
-        parsed = urlparse(url)
-        if parsed.scheme not in ('http', 'https') or not parsed.netloc:
-            return False
-
-        return True
-    except Exception:
-        return False
-
-@app.route('/shorten', methods=['POST'])
-def create_short_url():
-data = request.json
-original_url = data.get('url')
-if not original_url:
-return jsonify({"error": "URL is required"}), 400
-
-    # Check for valid URL
-    if not is_valid_url(original_url):
-        return jsonify({"error": "URL is invalid"}), 400
-
-       # Check if URL already exists
-    # Check if URL already exists
-existing = db.urls.find_one({"url": original_url})
-if existing:
-existing["id"] = str(existing["_id"])
-@@ -53,6 +72,40 @@ def create_short_url():
 url_data["id"] = str(inserted.inserted_id)
 
 return jsonify(url_data), 201
+# @app.route('/shorten', methods=['POST'])
+# def create_short_url():
+#     data = request.json
+#     original_url = data.get('url')
+#     if not original_url:
+#         return jsonify({"error": "URL is required"}), 400
+
+
+#        # Check if URL already exists
+#     existing = db.urls.find_one({"url": original_url})
+#     if existing:
+#         existing["id"] = str(existing["_id"])
+#         return jsonify({
+#             "message": "This URL has already been shortened.",
+#             "data": existing
+#         }), 200
+
+#     # Generate unique short code
+#     short_code = generate_short_code()
+#     while db.urls.find_one({"shortCode": short_code}):
+#         short_code = generate_short_code()
+
+#     now = datetime.utcnow().isoformat()
+#     url_data = {
+#         "url": original_url,
+#         "shortCode": short_code,
+#         "createdAt": now,
+#         "updatedAt": now,
+#         "accessCount": 0
+#     }
+#     inserted = db.urls.insert_one(url_data)
+#     url_data["id"] = str(inserted.inserted_id)
+
+#     return jsonify(url_data), 201
 
 
 @app.route('/shorten/<short_code>', methods=['GET'])
+@@ -154,5 +120,14 @@ def get_stats(short_code):
+url["id"] = str(url["_id"])
+return jsonify(url)
+
+@app.route('/r/<short_code>')
+def redirect_short_url(short_code):
+    url = db.urls.find_one({"shortCode": short_code})
+    if not url:
+        return render_template('404.html'), 404
+
+    db.urls.update_one({"shortCode": short_code}, {"$inc": {"accessCount": 1}})
+    return redirect(url['url'])
+
+if __name__ == '__main__':
+app.run(debug=True, port=5000)
