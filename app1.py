663ad0686cef8c537fbe5025b7285c68d3e6a60c
Tooba-Eman <emantooba554@gmail.com>
2025-05-25 11:29:33 +0500

    Added Update URL logic

--- app1.py
+++ app1.py
@@ -1,18 +1,27 @@
-url_data["id"] = str(inserted.inserted_id)
+url['id'] = str(url['_id'])
+return jsonify(url)
 
-return jsonify(url_data), 201
 
-# retrive Long Url by entring short code
-@app.route('/shorten/<short_code>', methods=['GET'])
-def retrieve_url(short_code):
+# Update Long URL based on short url
+@app.route('/shorten/<short_code>', methods=['PUT'])
+def update_url(short_code):
+    data = request.json
+    new_url = data.get("url")
+    if not new_url:
+        return jsonify({"error": "URL is required"}), 400
+
     url = db.urls.find_one({"shortCode": short_code})
     if not url:
         return jsonify({"error": "Short URL not found"}), 404
 
-    db.urls.update_one({"shortCode": short_code}, {"$inc": {"accessCount": 1}})
-    url['id'] = str(url['_id'])
-    return jsonify(url)
+    db.urls.update_one(
+        {"shortCode": short_code},
+        {"$set": {"url": new_url, "updatedAt": datetime.utcnow().isoformat()}}
+    )
+
+    updated = db.urls.find_one({"shortCode": short_code})
+    updated["id"] = str(updated["_id"])
+    return jsonify(updated)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
-
+app.run(debug=True, port=5000)
